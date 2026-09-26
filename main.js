5cf3230ec8a56624d3cee372f25e8a080b432755
Tobias <tobias.hanevik@outlook.com>
2026-09-26 14:51:37 +0200

    Rehydration of state
    - Added a small function that rehydrates the state on boot from main.js.
    
    - Checks for token and current user in localStorage and updates the state accordingly
    
    - If any of these are incorrect/expired, any API calls will return a failed reponse and content/data locked behind auth wont be accessible for the user anyways
    
    - Wire up redirection to login page when this scenario occurs later on

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import { Router } from './src/js/routing/router.js';
 import { routes } from './src/js/routing/routes.js';
 import { mainContainer } from './src/js/constants.js';
+import { hydrateState } from './src/js/state/manageState.js';
 
 const contentElement = mainContainer;
 
 const router = new Router(routes, contentElement);
+
+hydrateState();
 
 router.resolveRoute();
--- src/js/state/manageState.js
+++ src/js/state/manageState.js
@@ -1,11 +1,24 @@
 import { registerUser, loginUser } from '../services/authService.js';
-import { writeToken, deleteToken, writeCurrentUser, deleteCurrentUser } from '../storage/store.js';
+import { writeToken, readToken, deleteToken, writeCurrentUser, readCurrentUser, deleteCurrentUser } from '../storage/store.js';
 
 export const state = {
     token: null,
     currentUser: null,
     isLoggedIn: false,
 };
+
+export function hydrateState() {
+
+    const token = readToken();
+    const currentUser = readCurrentUser();
+
+    if (currentUser && token) {
+
+        state.token = token;
+        state.currentUser = currentUser;
+        state.isLoggedIn = true;
+    }
+}
 
 export async function register(userInput) {
     try {
